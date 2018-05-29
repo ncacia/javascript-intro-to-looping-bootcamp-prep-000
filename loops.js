7bcea4a7e42a0baaa8878fc363c309135b8ed6b9
Nicole Tams <nicole.cacia@gmail.com>
2018-05-29 01:23:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,27 +1,43 @@
-function forLoop (array){
-  for (var i = 0; i <25; i++){
-    array.push(`"I am ${i} strange loop ${i === 0 ?'' :'s'}."`)
+var arr = [];
+
+function forLoop(arr){
+for (let i = 0; i < 25; i++) {
+if (i === 1) {
+  arr.push("I am 1 strange loop.");
+  
+}
+  else 
+  {
+    arr.push("I am ${i} strange loops");
   }
-    return array
 }
 
-var n = 100;
-
-  function whileLoop(n){
-          while (n>0){
-            console.log(n--);
-            if (n===0)
-              return "done"
-          }
+return arr;
 }
 
+var n = 50;
+
+function whileLoop(n){
+
+while (n >= 0){
+  console.log(n);
+  n--;
+  if (n < 1){
+    return "done";
+  }
+}
+}
+
+var array = [1,2,3,4,5];
 function doWhileLoop(array) {
-  function maybeTrue(){
-    return Math.random() >= 0.5
-}
-do{
-  array.pop()
-}while (array.length > 0 && maybeTrue());
 
-return array
+do{
+ array.shift();
+
+} while(array.length > 0 && maybeTrue());
+
+{
+return array;
 }
+}
+
